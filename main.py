9f004f46c8a9db713815326a710105b3e5e7d29e
Dmitry Mordanov <mordanov314@gmail.com>
2023-12-14 14:02:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Вводный курс по Big Data (семинары)
+
+# Урок 3. Инструменты работы и визуализации
+
+def get_data_from_kaggle():
+    pass
+
+def transform_data():
+    pass
+
+def save_data_to_csv():
+    pass
+
+def load_to_hadoop():
+    pass
+
+def load_to_hive():
+    pass
+
+def data_analysis():
+    pass
+
+if __name__ == '__main__':
+    print('PyCharm')
+    get_data_from_kaggle()
+    transform_data()
+    save_data_to_csv()
+    load_to_hadoop()
+    load_to_hive()
+    data_analysis()
